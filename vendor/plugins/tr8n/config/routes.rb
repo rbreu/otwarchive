5c997981e68fae6a5b0a78bda3ddc8e33535c166
Rebecca <rebecca@rbreu.de>
2011-02-22 22:25:50 +0100

    remove tr8n awards

--- vendor/plugins/tr8n/config/routes.rb
+++ vendor/plugins/tr8n/config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  [:awards, :chart, :dashboard, :language_cases, 
+  [:chart, :dashboard, :language_cases, 
    :language, :phrases, :translations, :translator, :login].each do |ctrl|   
     map.connect "tr8n/#{ctrl}/:action", :controller => "tr8n/#{ctrl}"
   end
